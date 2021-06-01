ec0a59dc8cf076f8916e733a3961ce7b2c69d91f
KauanValle <kauanvalle3@gmail.com>
2021-06-01 15:42:27 -0300

    100% Finalizado

--- js/js.js
+++ js/js.js
@@ -10,24 +10,23 @@ function changeColor(){
     var mode = document.getElementById('radio2')
     var container2 = document.getElementsByClassName('container2')[0]
 
-    if(colorSwitch.checked){
-        corpo.style = 'background-color: #111;'
-        titulo.style = 'color: white;'
-        contaErrada.style.color = 'white'
-        iphone.src = 'css/images/iphone-preto.png'
-        colorSwitch.style = 'background-color: black;'
-        mode.style = 'color: white;'
-        mode.innerHTML = 'LIGHTMODE'
-        radio1.style = 'background: url(css/images/sun_30px.png) no-repeat; transition: 0.5s;'
-        container2.style = 'border: 1px solid #555;'
-    }else{
-        corpo.style = 'background-color: white;'
-        titulo.style = 'color: black;'
-        contaErrada.style.color = 'black'
-        iphone.src = 'css/images/iphone-branco.png'
-        mode.style = 'color: black;'
-        mode.innerHTML = 'DARKMODE'
-        radio1.style = 'background: url(css/images/moon_symbol_30px.png) no-repeat; transition: 0.5s;'
-        container2.style = 'border: 1px solid #DDD;'
+    if(colorSwitch.checked){//-----------------------------DARKMODE------------------------------
+        corpo.style = 'background-color: #111;' //Background grey
+        titulo.style = 'color: white;' // 'Instagram' white
+        contaErrada.style.color = 'white' // 'Não é cwb_kauan?' white
+        iphone.src = 'css/images/iphone-preto.png' // Iphone black image
+        mode.style = 'color: white;' // 'LIGHTMODE' white
+        mode.innerHTML = 'LIGHTMODE' // Writing lightmode
+        radio1.style = 'background: url(css/images/sun_30px.png) no-repeat; transition: 0.5s;' // Add sun image and transition
+        container2.style = 'border: 1px solid #555;' // Add border dark grey around main container
+    }else{//-----------------------------LIGHTMODE------------------------------
+        corpo.style = 'background-color: white;' //Background white
+        titulo.style = 'color: black;' // 'Instagram' black
+        contaErrada.style.color = 'black' // 'Não é cwb_kauan?' black
+        iphone.src = 'css/images/iphone-branco.png' // Iphone white image
+        mode.style = 'color: black;' // 'DARKMODE' black
+        mode.innerHTML = 'DARKMODE' // Writing lightmode
+        radio1.style = 'background: url(css/images/moon_symbol_30px.png) no-repeat; transition: 0.5s;' // Add moon image and transition
+        container2.style = 'border: 1px solid #DDD;' // Add border light grey around main container
     }
 }
